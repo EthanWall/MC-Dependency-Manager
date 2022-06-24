75f34a0aa932ed66d6a7924d37ae30066d4ea016
Ethan Wall <53667981+EthanWall@users.noreply.github.com>
2022-06-23 22:22:28 -0400

    Began migrating functions to util file

---
+++ src/errors.ts
@@ -0,0 +1,29 @@
+import {Mod} from "node-curseforge";
+
+export class ModFileNotFoundError extends Error {
+    readonly modSlug: string;
+    readonly gameVersion: string;
+    readonly modLoader: string;
+
+    constructor(mod: Mod, gameVersion: string, modLoader: string) {
+        super(`Could not find file for ${mod.slug} on game version ${gameVersion}-${modLoader}`);
+
+        Object.setPrototypeOf(this, ModFileNotFoundError.prototype);
+
+        this.modSlug = mod.slug;
+        this.gameVersion = gameVersion;
+        this.modLoader = modLoader;
+    }
+}
+
+export class ModNotFoundError extends Error {
+    readonly modSlug: string;
+
+    constructor(slug: string) {
+        super(`Could not find a mod with the slug ${slug}`);
+
+        Object.setPrototypeOf(this, ModNotFoundError.prototype);
+
+        this.modSlug = slug;
+    }
+}
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,8 @@
-import {Mod} from "node-curseforge";
+import {Curseforge, Game, Mod, ModFile} from "node-curseforge";
+import {ModLoaderType} from "node-curseforge/dist/objects/enums";
+import {ModFileNotFoundError, ModNotFoundError} from "./errors";
+
+const MODS_CLASS_ID = 6;
 
 export function sortModsSearch(index: Mod[], query: string): Mod[] {
     const formattedQuery = query.toLowerCase();
@@ -16,4 +20,44 @@ export function sortModsSearch(index: Mod[], query: string): Mod[] {
         return entry;
         // @ts-ignore
     }).sort((a, b) => b.points - a.points);
+}
+
+export async function getDependencies(modFile: ModFile, cf: Curseforge): Promise<Mod[]> {
+    // Get the mod ID's of the dependencies
+    const ids = modFile.dependencies.map(dep => dep.modId);
+
+    // Get a Mod for each mod ID
+    return await Promise.all(ids.map(id => cf.get_mod(id)));
+}
+
+export async function getLatestModFile(mod: Mod, gameVersion: string, modLoader: ModLoaderType): Promise<ModFile> {
+    const options = {
+        gameVersion,
+        modLoaderType: modLoader,
+        pageSize: 1
+    };
+
+    const files = await mod.get_files(options);
+
+    // Throw an error if a file doesn't exist
+    if (!files[0]) {
+        throw new ModFileNotFoundError(mod, gameVersion, ModLoaderType[modLoader]);
+    }
+
+    return files[0];
+}
+
+export async function getModFromSlug(slug: string, mc: Game): Promise<Mod> {
+    let options = {
+        classId: MODS_CLASS_ID,
+        slug
+    };
+
+    const mods = await mc.search_mods(options);
+
+    if (!mods[0]) {
+        throw new ModNotFoundError(slug);
+    }
+
+    return mods[0];
 }
