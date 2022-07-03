42c8bdbbe92998731667920e8ad1d2d51bbb895e
Ethan Wall <53667981+EthanWall@users.noreply.github.com>
2022-07-03 12:01:29 -0400

    IT WORKS

--- src/mcmm-remove.ts
+++ src/mcmm-remove.ts
@@ -1,77 +1,63 @@
 import {getPackages, PackageIndex, removePackage} from "./files";
 
-export async function cmdRemove(slugs: Array<string>) {
-    // TODO: Remove multiple mods at a time
-
+export async function cmdRemove(userSlugs: Array<string>) {
     const packages = await getPackages();
-    await removeOne(slugs[0], packages);
-}
 
-async function removeOne(slug: string, packages: PackageIndex): Promise<boolean> {
-    let dependencies;
+    // Find a unique list of all packages in the removal chain
+    let chainSlugs;
     try {
-        // Get the mod's dependencies
-        // There may be duplicates, due to recursion
-        // i.e. A depends on W and X. X also depends on W. ['W', 'X', 'W'] is returned
-        dependencies = getPackageDependencies(slug, packages, {recursive: true});
+        chainSlugs = userSlugs.concat(...userSlugs.flatMap(slug => getPackageDependencies(slug, packages, {recursive: true})));
     } catch (err) {
-        if (err instanceof RangeError) {
-            console.error(`${slug} has a cyclical dependency (i.e. A depends on B and B depends on A)! Please fix this`);
-            return false;
+        if (err instanceof TypeError) {
+            console.error('malformed JSON');
+        } else if (err instanceof RangeError) {
+            console.error('circular dependencies are not supported');
+        } else {
+            throw err;
         }
-        throw err;
+        return;
+    }
+    chainSlugs = [...new Set(chainSlugs)];
+
+    // Mark all mods in the command arguments as non-user mods
+    // This is needed as user mods can't be removed
+    userSlugs.forEach(slug => packages[slug].userMod = false);
+
+    // Remove all user mods from the list of packages to be removed
+    for (let i = 0; i < chainSlugs.length; i++) {
+        const slug = chainSlugs[i];
+        if (packages[slug].userMod)
+            chainSlugs.splice(i, 1);
     }
 
-    // A unique set of dependencies that will be removed in the final step
-    const dependenciesToRemove = new Set(dependencies);
+    // Find the number of times each package is referenced as a dependency
+    const refCounts: { [slug: string]: number } = {};
+    chainSlugs.forEach(slug => refCounts[slug] = getReferences(slug, packages).length);
 
-    // Check if we're trying to remove a dependency of another mod
-    if (getRefCount(slug, packages) !== 0) {
-        console.error(`Another mod relies on ${slug}`);
-        return false;
+    // Iterate over each package
+    chainSlugs.forEach(slug => {
+        // Find shallow dependencies
+        const dependencies = getPackageDependencies(slug, packages, {recursive: false});
+
+        // Decrement the reference count of each dependency
+        dependencies.forEach(dep => {
+            if (refCounts.hasOwnProperty(dep))
+                refCounts[dep]--;
+        });
+    });
+
+    // Remove all packages with a reference count of zero
+    chainSlugs = chainSlugs.filter(slug => refCounts[slug] === 0);
+    for (const slug of chainSlugs) {
+        await removePackage(slug);
     }
 
-    for (const primaryDep of dependenciesToRemove) {
-        // Check if the dependency is a user-installed mod
-        if (packages[primaryDep].userMod) {
-            // If so, gather a list of its dependencies, as they mustn't be removed
-            const subDeps = getPackageDependencies(primaryDep, packages, {recursive: true});
-
-            // Remove the user-install dependency and its dependencies
-            subDeps.concat(primaryDep).forEach(userMod => dependenciesToRemove.delete(userMod));
-            continue;
-        }
-
-        // Ensure no other mods are dependent on those dependencies
-        // Get number of mods that declare each primary dependencies as a dependency of their own
-        const totalReferences = getRefCount(primaryDep, packages);
-
-        // Get number of times the primary mod depends on the dependency
-        const ourReferences = dependencies.filter(name => name === primaryDep).length;
-
-        // Check if the number of references to each dep from this mod isn't equal to the total number of references to the dep
-        if (ourReferences !== totalReferences)
-            // If there are dependents, don't remove them
-            dependenciesToRemove.delete(primaryDep);
-    }
-
-    // Remove the package and its eligible dependencies
-    for (const dep of [...dependenciesToRemove].concat(slug)) {
-        await removePackage(dep);
-    }
-    console.log(`Removed ${[slug].concat(...dependenciesToRemove).join(', ')}`);
-
-    return true;
+    console.log(`Removed ${chainSlugs.length ? chainSlugs.join(', ') : 'nothing'}`);
 }
 
-/**
- * The number of entries that list this slug as a dependency
- * @param slug The mod ID
- * @param packages Named index of packages
- */
-function getRefCount(slug: string, packages: PackageIndex): number {
-    const depSlugs = Object.values(packages).flatMap(pkg => pkg.dependencies ?? []);
-    return depSlugs.filter(str => str === slug).length;
+function getReferences(slug: string, packages: PackageIndex): string[] {
+    const allDeps = Object.values(packages).flatMap(pkg => pkg.dependencies ?? []);
+    return allDeps.filter(str => str === slug);
 }
 
 function getPackageDependencies(slug: string, packages: PackageIndex, options?: { recursive?: boolean }): string[] {
